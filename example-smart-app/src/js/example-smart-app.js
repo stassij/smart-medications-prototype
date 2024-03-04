b4647c37fe730428a8861b8b76ae457edbe10a79
John Stassi <jbstassi@gmail.com>
2024-03-03 22:10:32 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -24,7 +24,7 @@
         $.when(pt, meds).fail(onError);
 
         $.when(pt, meds).done(function(patient, medications) {
-          //var byCodes = smart.byCodes(obv, 'code');
+          
           console.log(patient);
           var name = '';
           var fname = '';
@@ -35,12 +35,6 @@
             lname = patient.name[0].family.join(' ');
           }
           var gender = patient.gender;
-
-          //var height = byCodes('8302-2');
-          //var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          //var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          //var hdl = byCodes('2085-9');
-          //var ldl = byCodes('2089-1');
           
           var p = defaultPatient();
           p.name = name;
@@ -49,21 +43,7 @@
           p.gender = gender;
           p.birthdate = patient.birthDate;
           
-          //p.height = getQuantityValueAndUnit(height[0]);
-
-          //if (typeof systolicbp != 'undefined')  {
-          //  p.systolicbp = systolicbp;
-          //}
-
-          //if (typeof diastolicbp != 'undefined') {
-          //  p.diastolicbp = diastolicbp;
-          //}
-
-          //p.hdl = getQuantityValueAndUnit(hdl[0]);
-          //p.ldl = getQuantityValueAndUnit(ldl[0]);
-          
           populateActiveMedicationTable(medications);
-          //populateObservationTable(observations);
           
           ret.resolve(p);
         });
@@ -83,12 +63,7 @@
       fname: {value: ''},
       lname: {value: ''},
       gender: {value: ''},
-      birthdate: {value: ''},
-      //height: {value: ''},
-      //systolicbp: {value: ''},
-      //diastolicbp: {value: ''},
-      //ldl: {value: ''},
-      //hdl: {value: ''},
+      birthdate: {value: ''}
     };
   }
 
@@ -155,11 +130,6 @@
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
-    //$('#height').html(p.height);
-    //$('#systolicbp').html(p.systolicbp);
-    //$('#diastolicbp').html(p.diastolicbp);
-    //$('#ldl').html(p.ldl);
-    //$('#hdl').html(p.hdl);
   };
 
 })(window);
