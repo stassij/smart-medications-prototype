f5e4ba40bff7718b07be482de55cedc831cafb82
John Stassi <jbstassi@gmail.com>
2024-03-03 22:30:47 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -64,13 +64,13 @@
 
   function populateActiveMedicationTable(meds){
     $('#medsTable').empty();
-    $('#medsTable').append("<tr><th>ACTIVE MEDICATION</th><th>Status</th><th>Date Written</th><th>Date Ended</th></tr>");
+    $('#medsTable').append("<tr><th>ACTIVE MEDICATION</th><th>Prescriber</th><th>Status</th><th>Date Written</th><th>Date Ended</th></tr>");
     
     for(var i in meds){
       var med = meds[i]
       console.log(med)
       if(med.status == 'active'){
-        var row = "<tr><td>" + med.medicationCodeableConcept.text + "</td><td>" + med.status + "</td><td>" + med.dateWritten + "</td><td>" + med.dateEnded + "</td></tr>";
+        var row = "<tr><td>" + med.medicationCodeableConcept.text + "</td><td>" + med.prescriber.name[0].text + "</td><td>" + med.status + "</td><td>" + med.dateWritten + "</td><td>" + med.dateEnded + "</td></tr>";
         $('#medsTable').append(row);
       }
     }
